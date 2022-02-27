10194ad3d7219aabe273ab02d25b09ce2c3abf00
Madang <chooi9522@gmail.com>
2022-02-27 20:06:19 +0900

    Edit Home screen css & Add Item html

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,24 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="flex flex-col space-y-5 py-10">
       {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-        <div key={i} className="flex cursor-pointer border-b pb-4">
-          <div>
-            <div />
-            <div>
-              <h3>New iPhone 14</h3>
-              <span>Black</span>
-              <span>$95</span>
+        <div
+          key={i}
+          className="flex cursor-pointer justify-between border-b px-4 pb-5"
+        >
+          <div className="flex space-x-4">
+            <div className="h-20 w-20 rounded-md bg-gray-400" />
+            <div className="flex flex-col pt-2">
+              <h3 className="text-sm font-medium text-gray-900">
+                New iPhone 14
+              </h3>
+              <span className="text-xs text-gray-500">Black</span>
+              <span className="mt-1 font-medium text-gray-900">$95</span>
             </div>
           </div>
-          <div>
-            <div>
+          <div className="flex items-end justify-end space-x-2">
+            <div className="flex items-center space-x-0.5 text-sm text-gray-600">
               <svg
                 className="h-4 w-4"
                 fill="none"
@@ -31,7 +36,7 @@ const Home: NextPage = () => {
               </svg>
               <span>1</span>
             </div>
-            <div>
+            <div className="flex items-center space-x-0.5 text-sm text-gray-600">
               <svg
                 className="h-4 w-4"
                 fill="none"
@@ -51,7 +56,7 @@ const Home: NextPage = () => {
           </div>
         </div>
       ))}
-      <button>
+      <button className="rigth-5 shadowxl fixed bottom-24 cursor-pointer rounded-full bg-orange-400 p-4 text-white transition-colors hover:bg-orange-500">
         <svg
           className="h-6 w-6"
           xmlns="http://www.w3.org/2000/svg"
