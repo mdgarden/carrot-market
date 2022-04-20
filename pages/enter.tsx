ec463d11c83c7b5ed143ad8a91f33cf7a4a15c26
Madang <chooi9522@gmail.com>
2022-04-20 20:33:37 +0900

    Add enter page

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,26 +1,25 @@
 import type { NextPage } from "next";
 import { useState } from "react";
-
-function cls(...classnames: string[]) {
-  return classnames.join(" ");
-}
+import Button from "../components/button";
+import Input from "../components/input";
+import { cls } from "../libs/utils";
 
 const Enter: NextPage = () => {
   const [method, setMethod] = useState<"email" | "phone">("email");
   const onEmailClick = () => setMethod("email");
   const onPhoneClick = () => setMethod("phone");
   return (
-    <div className="mt-16">
+    <div className="mt-16 px-4">
       <h3 className="text-center text-3xl font-bold">Enter to Carrot</h3>
-      <div className="mt-8">
+      <div className="mt-12">
         <div className="flex flex-col items-center">
           <h5 className="text-sm font-medium text-gray-500">Enter using:</h5>
-          <div className="mt-8 grid w-full grid-cols-2 gap-16 border-b pb-4">
+          <div className="mt-8  grid  w-full grid-cols-2 border-b ">
             <button
               className={cls(
                 "border-b-2 pb-4 text-sm font-medium",
                 method === "email"
-                  ? "border-orange-500 text-orange-400"
+                  ? " border-orange-500 text-orange-400"
                   : "border-transparent text-gray-500 hover:text-gray-400"
               )}
               onClick={onEmailClick}
@@ -31,7 +30,7 @@ const Enter: NextPage = () => {
               className={cls(
                 "border-b-2 pb-4 text-sm font-medium",
                 method === "phone"
-                  ? "border-orange-500 text-orange-400"
+                  ? " border-orange-500 text-orange-400"
                   : "border-transparent text-gray-500 hover:text-gray-400"
               )}
               onClick={onPhoneClick}
@@ -40,37 +39,25 @@ const Enter: NextPage = () => {
             </button>
           </div>
         </div>
-        <form className="mt-8 flex flex-col">
-          <label className="text-sm font-medium text-gray-700">
-            {method === "email" ? "Email address" : null}
-            {method === "phone" ? "Phone number" : null}
-          </label>
-          <div className="mt-1">
-            {method === "email" ? (
-              <input
-                type="email"
-                className="w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-orange-500 focus:outline-none"
-                required
-              />
-            ) : null}
-            {method === "phone" ? (
-              <div className="flex rounded-md shadow-sm">
-                <span className="flex select-none items-center justify-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-sm text-gray-500">
-                  +82
-                </span>
-                <input
-                  type="number"
-                  className="w-full appearance-none rounded-md rounded-l-none border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
-                  required
-                />
-              </div>
-            ) : null}
-          </div>
-          <button className="mt-5 rounded-md border border-transparent bg-orange-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-orange-600 focus:outline-none focus:ring-orange-500">
-            {method === "email" ? "Get login link" : null}
-            {method === "phone" ? "Get one-time password" : null}
-          </button>
+        <form className="mt-8 flex flex-col space-y-4">
+          {method === "email" ? (
+            <Input name="email" label="Email address" type="email" required />
+          ) : null}
+          {method === "phone" ? (
+            <Input
+              name="phone"
+              label="Phone number"
+              type="number"
+              kind="phone"
+              required
+            />
+          ) : null}
+          {method === "email" ? <Button text={"Get login link"} /> : null}
+          {method === "phone" ? (
+            <Button text={"Get one-time password"} />
+          ) : null}
         </form>
+
         <div className="mt-8">
           <div className="relative">
             <div className="absolute w-full border-t border-gray-300" />
@@ -111,5 +98,4 @@ const Enter: NextPage = () => {
     </div>
   );
 };
-
 export default Enter;
