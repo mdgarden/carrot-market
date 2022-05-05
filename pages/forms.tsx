25b2daacc35ecd3dfb1bc06971d41eda973c01d0
Madang <chooi9522@gmail.com>
2022-05-05 21:10:07 +0900

    react hook form validation

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -6,14 +6,29 @@ import { useForm } from "react-hook-form";
 // Easier Inputs
 
 export default function Forms() {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const { register, handleSubmit } = useForm();
+  const onValid = () => {
+    console.log("valid");
+  };
+
   return (
-    <form>
-      <input {...register("username")} type="text" placeholder="Username" />
-      <input {...register("email")} type="email" placeholder="Email" />
-      <input {...register("password")} type="password" placeholder="Password" />
-      <input type="submit" />
+    <form onSubmit={handleSubmit(onValid)}>
+      <input
+        {...register("username", { required: true })}
+        type="text"
+        placeholder="Username"
+      />
+      <input
+        {...register("email", { required: true })}
+        type="email"
+        placeholder="Email"
+      />
+      <input
+        {...register("password", { required: true })}
+        type="password"
+        placeholder="Password"
+      />
+      <input type="submit" value="Create Account" />
     </form>
   );
 }
