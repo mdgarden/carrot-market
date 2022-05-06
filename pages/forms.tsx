094038481adca12b4dc6a72be5c8049cc94c5d03
Madang <chooi9522@gmail.com>
2022-05-06 21:20:22 +0900

    form validation setting

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,20 +1,35 @@
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
 
 // Goals with useForm: Less Code, Better validation, Better Errors(set, clear, display)
 // Have control over inputs
 // Don't deal with events
 // Easier Inputs
 
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+}
+
 export default function Forms() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit } = useForm<LoginForm>();
   const onValid = () => {
     console.log("valid");
+  };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
   };
 
   return (
     <form onSubmit={handleSubmit(onValid)}>
       <input
-        {...register("username", { required: true })}
+        {...register("username", {
+          required: "User Name is Required",
+          minLength: {
+            message: "The user name should be longer than 5 chars.",
+            value: 5,
+          },
+        })}
         type="text"
         placeholder="Username"
       />
