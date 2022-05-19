488261eb3ad366c179eefe9ee1bac2e9ba8c5f3a
Madang <chooi9522@gmail.com>
2022-05-19 23:43:41 +0900

    Edit Api file

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,7 +3,7 @@ import withHandler from "../../../libs/server/withHandler";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body);
-  res.status(200).end();
+  return res.status(200).end();
 }
 
 export default withHandler("POST", handler);
