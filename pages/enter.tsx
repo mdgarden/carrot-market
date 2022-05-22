373b4eb5b8963b378928cff2c32a2a85a1dfa6ad
Madang <chooi9522@gmail.com>
2022-05-22 23:53:11 +0900

    Edit tscofing file path

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -3,8 +3,8 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import Button from "@components/button";
 import Input from "@components/input";
-import useMutation from "../libs/client/useMutation";
-import { cls } from "../libs/client/utils";
+import useMutation from "@libs/client/useMutation";
+import { cls } from "@libs/client/utils";
 
 interface EnterForm {
   email?: string;
