61b5e3f736ea933e689a50893f5bbf723f7d2045
Madang <chooi9522@gmail.com>
2022-06-23 00:46:38 +0900

    Token Login clean code

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,31 +1,28 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler from "@libs/server/withHandler";
 import client from "@libs/server/client";
-import { prisma } from "@prisma/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
-  const payload = phone ? { phone: +phone } : { email };
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
-    },
-    create: {
-      name: "Anonymous",
-      ...payload,
-    },
-    update: {},
-  });
+  const user = phone ? { phone: +phone } : { email };
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
   const token = await client.token.create({
     data: {
-      payload: "1234",
+      payload,
       user: {
-        connect: {
-          id: user.id,
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymous",
+            ...user,
+          },
         },
       },
     },
   });
+  console.log(token);
 
   return res.status(200).end();
 }
